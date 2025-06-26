19c8ec72921b84a4b42ca690f7d64448e4f1c333
Kenny Anderson <61395858+skyl33tg3208@users.noreply.github.com>
2025-06-26 08:39:06 -0700

    Delete Ternary Operator.py

--- Ternary Operator.py
+++
@@ -1,8 +0,0 @@
-# Ternary Operator is a short hand way to evaluate if a condition is true or false
-# condition_if_true if condition else condition_if_else
-
-she_likes_you = False
-
-will_date = "she will go on a date" if she_likes_you else "you will get denied"
-
-print(will_date)
